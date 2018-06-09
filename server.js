9797304cb1fb8f017929cd0c815e4d9bcc9075de
Glitch (succinct-loan) <none>
2018-06-09 11:41:45 +0000

    🍫🙎 Checkpoint ./server.js:1286339/345 ./package.json:1286339/1366

--- server.js
+++ server.js
@@ -4,16 +4,19 @@
 // init project
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(bodyParser.json())
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/:date_string", function (request, response) {
+    let dateString = request.params.date_string;
+    
+    response.send({unix: , utc:   });
 });
 
 // listen for requests :)
